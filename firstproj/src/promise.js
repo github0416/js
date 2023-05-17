d689c9820fdd05ffca7807fabb9ea57377e3680f
Eric Chen <lingxun.chen@gmail.com>
2023-05-17 15:02:29 -0500

    promise is resolved only once; let var init timing

--- firstproj/src/promise.js
+++ firstproj/src/promise.js
@@ -1,21 +1,30 @@
 var p = new Promise(
+    
+
     (resolve, reject) => {
-        console.log("invoked");
+        let b;
+
+        console.log("invoked with: " + b); // line #1
 //        setTimeout(square=(x) => {console.log("defining square"); resolve(x*x); }, 10000)
-        setTimeout((x) => {console.log("defining square"); resolve(x*x); }, 1, 5)
+        setTimeout(triple = (x) => {console.log("defining triple: " + b); resolve(b*x); }, 10000, 6)
+
+        b = 3;
     }
+
 );
 
 console.log("promise created");
 
-p.then((x) => { console.log(`squared: ${x}`); });
+p.then((x) => { console.log(`tripled: ${x}`); });
 
 // console.log(square);
 
-// square(5);
+triple(5);
 
 C:\Program Files\nodejs\node.exe .\src\promise.js
-invoked                <-- the function parameter passed to Promise constructor is immediately invoked
-promise created        <-- starts executing synchronous stmts
-defining square        <-- async op in promise is executed
-squared: 25            <-- promise resolve part is done
+invoked with: undefined     <-- b's value not available yet at line #1
+promise created
+defining triple: 3          <-- b's value is available when triple() is executed
+tripled: 15
+defining triple: 3          <-- promise is only resolved once, so no "tripled: 18" is logged
+
